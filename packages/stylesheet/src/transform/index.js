5e0033dc21218970c50fdb43ece418c596124465
Sophon <sophonmen50@gmail.com>
2021-11-16 20:50:59 +0700

    [UNTRACK] add add wrapStyle

--- packages/stylesheet/src/transform/index.js
+++ packages/stylesheet/src/transform/index.js
@@ -36,6 +36,7 @@ const transformStyle = (key, value, container) => {
 const transform = (container) => (style) => {
   if (!style) return style;
 
+
   const propsArray = Object.keys(style);
   const resolvedStyle = {};
 
@@ -46,7 +47,6 @@ const transform = (container) => (style) => {
 
     resolvedStyle[key] = transformed;
   }
-
   return resolvedStyle;
 };
 
